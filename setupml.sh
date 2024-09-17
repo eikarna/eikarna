868ee621768097e213ef1c0be4c2f6ac7ec87a76
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-09-17 21:35:51 +0700

    Update setupml.sh
    Add sudo

--- setupml.sh
+++ setupml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt update -y &> /dev/null
-apt install wget clang -y &> /dev/null
+sudo apt update -y &> /dev/null
+sudo apt install wget clang -y &> /dev/null
 wget https://raw.githubusercontent.com/eikarna/eikarna/main/mludp.c &> /dev/null
 clang -Ofast -o mludp mludp.c
 wget https://raw.githubusercontent.com/eikarna/eikarna/main/mludp2.c &> /dev/null
