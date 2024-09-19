3e0a38af16326b5c97df6bcd6882a53e162b0b40
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-09-19 07:40:02 +0700

    Update setupml.sh

--- setupml.sh
+++ setupml.sh
@@ -6,3 +6,8 @@ wget https://raw.githubusercontent.com/eikarna/eikarna/main/mludp.c &> /dev/null
 clang -Ofast -o mludp mludp.c
 wget https://raw.githubusercontent.com/eikarna/eikarna/main/mludp2.c &> /dev/null
 clang -Ofast -o mludp2 mludp2.c
+
+# Set ulimit globally
+for opt in $(ulimit -a | sed 's/.*\-\([a-z]\)[^a-zA-Z].*$/\1/'); do
+     ulimit -$opt ulimited
+ done
